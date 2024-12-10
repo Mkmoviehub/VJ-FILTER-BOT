8e223f4f5865d8bbf3a5591044f19d26431f62cd
Mkmoviehub <kondapallishiva143@gmail.com>
2024-12-10 14:58:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -82,8 +82,8 @@ PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b>- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟ�
 
 
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
-CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
-CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://kondapallishiva1431:3GNh3NTP3bhAOTsi@cluster0.kj2e8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Necessary If clone mode is true
+CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
+CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone mode is true
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'mkmoviesfile') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 

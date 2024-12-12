8eea004bac6bf32119ae6c55c8b641e71cddc8de
Mkmoviehub <kondapallishiva143@gmail.com>
2024-12-12 21:44:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -163,7 +163,7 @@ MULTI_CLIENT = False
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
-    ON_HEROKU = False 
+    ON_HEROKU = True 
 else:
     ON_HEROKU = False 
 URL = environ.get("URL", "https://mass-ellette-mkmovieshub-799c811b.koyeb.app/")

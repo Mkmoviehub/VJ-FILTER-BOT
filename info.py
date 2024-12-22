fb81338077f3a72cfcde6e7d484d7adcf85bff38
Mkmoviehub <kondapallishiva143@gmail.com>
2024-12-22 10:58:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -91,7 +91,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'mkmoviesfile') # Publi
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/mkmovies_hub')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/mkmovies_hub')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'mk_movie_request') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'http://t.me/MKMOVIESHUB_bot/mk1')
+OWNER_LNK = environ.get('NEW_MOVIES', 'http://t.me/MKMOVIESHUB_bot/mk1')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
